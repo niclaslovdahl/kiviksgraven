800dca75fe9e8a54636e4e7cfda074228dbf044e
Johan Månsson <mansson.johan@me.com>
2016-12-09 16:10:24 +0100

    minor changes

--- Assets/Scripts/SceneController.cs
+++ Assets/Scripts/SceneController.cs
@@ -1,16 +1,19 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.SceneManagement;
 
 public class SceneController : MonoBehaviour {
 	public float duration = 1.0F;
 	public Light light;
 	private bool changeLight = false;
+	private AsyncOperation scene;
 
 	public AudioClip sound2;
 	public GameObject procession;
 
     void Start() {
-
+		//scene = SceneManager.LoadSceneAsync("Menu", LoadSceneMode.Single);
+		//scene.allowSceneActivation = false;
 		iTween.FadeTo (procession, 0.0f, 0.0f);
 		StartCoroutine(Example());
 		
@@ -19,8 +22,6 @@ public class SceneController : MonoBehaviour {
     void FixedUpdate() {
 		if (changeLight) {
 
-			float phi = Time.time / duration * 2 * Mathf.PI;
-			float amplitude = Mathf.Cos(phi) * 0.5F + 0.5F;
 			light.intensity += 0.02F;
 
 		}
@@ -41,6 +42,9 @@ public class SceneController : MonoBehaviour {
 
 		yield return new WaitForSeconds(4);
 		iTween.FadeTo (procession, 0.0f, 2.0f);
+
+		yield return new WaitForSeconds(4);
+		//scene.allowSceneActivation = true;
 	
 
 	}
