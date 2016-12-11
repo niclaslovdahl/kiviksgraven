ef16ebbc4ffb804e107989ea188211cb2810b571
NiclasLovdahl <niclas.lovdahl@gmail.com>
2016-12-11 18:20:00 +0100

    flying up and down in First scene by holding controller over your head tested and works.

---
+++ Assets/Scripts/flyHigh.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class flyHigh : MonoBehaviour
+{
+
+    // Variables for speed and hight
+    private float minHeight = -0.01000381f;
+    private float maxHeight = 25f;
+    private float boxSpeed = 0.05f;
+
+    private MeshRenderer mesh;
+    private Transform trans;
+    private bool activated;
+    private GameObject obj;
+    private Transform boxTransform;
+
+    public Transform head;
+    public GameObject VRbox;
+
+    // Use this for initialization
+    void Start()
+    {
+        mesh = GetComponent<MeshRenderer>();
+        trans = GetComponent<Transform>();
+        boxTransform = VRbox.GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if ((trans.position.y > (head.position.y + 0.15f) && (boxTransform.transform.position.y <= maxHeight)))
+        {
+            boxTransform.Translate(0, boxSpeed, 0, Space.Self);
+        }
+        if ((trans.position.y <= head.position.y + 0.15f) && (boxTransform.transform.position.y >= minHeight))
+        {
+            boxTransform.Translate(0, -boxSpeed, 0, Space.Self);
+        }
+    }
+}
