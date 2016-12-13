5ff3fa81cfa56c736c96333a4354cddfcb1961d2
NiclasLovdahl <niclas.lovdahl@gmail.com>
2016-12-13 20:10:03 +0100

    minor fixes

--- Assets/sceneTriggerMenu.cs
+++ Assets/sceneTriggerMenu.cs
@@ -5,6 +5,7 @@ using UnityEngine.SceneManagement;
 public class sceneTriggerMenu : MonoBehaviour {
 
     public GameObject fader;
+    public GameObject footsteps;
 
     private AsyncOperation scene;
 
@@ -19,7 +20,9 @@ public class sceneTriggerMenu : MonoBehaviour {
 
     IEnumerator sceneChange() {
         iTween.FadeTo(fader, 1f, 5f);
-        yield return new WaitForSeconds(5);
+        yield return new WaitForSeconds(2);
+        footsteps.SetActive(false);
+        yield return new WaitForSeconds(3);
         scene.allowSceneActivation = true;
     }
 }
